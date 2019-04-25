35c4c9283041e57a7e334d126c3c64d55e2d0d4f
Vincent Mahnke <vincent@mahn.ke>
2019-04-25 12:05:19 +0200

    Initial commit

---
+++ 0_vs/volumeStates/volumeStates/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace volumeStates
+{
+    public partial class MainWindow : Window
+    {
+        AudioDevice currentlyActiveDevice;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            currentlyActiveDevice = AudioUtilities.GetDefaultSpeaker();
+
+            foreach (AudioSession session in AudioUtilities.GetAllSessions())
+            {
+                if (session.Process != null)
+                {
+                    // only the one associated with a defined process
+                    Console.WriteLine(session.Process.ProcessName + ": " + session.Volume);
+                }
+            }
+        }
+    }
+}
